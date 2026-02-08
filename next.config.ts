00859e890843c14292cb04ef689ee8c6ab338502
stanley-tech-code <stanmbugua6@gmail.com>
2026-02-08 17:14:01 +0300

    Simplify next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Vercel will auto-configure for deployment
 };
 
 export default nextConfig;
